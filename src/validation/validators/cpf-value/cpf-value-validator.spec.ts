8bf86715d41e6d93ddd5d20eb5aeb37df8179a76
Nilton F J <niltonfjr086@gmail.com>
2022-02-06 15:40:22 +0000

    test: implement logic to validate cpf values

---
+++ src/validation/validators/cpf-value/cpf-value-validator.spec.ts
@@ -0,0 +1,52 @@
+import { CpfValueValidator } from './cpf-value-validator';
+
+const makeSut = () => ({ sut: new CpfValueValidator() });
+
+describe('CpfValueValidator unit test', () => {
+  it('should be defined', () => {
+    const { sut } = makeSut();
+    expect(sut).toBeDefined();
+  });
+  it('GIVEN a sut instance WHEN validating an invalid CPF value THEN should return false', () => {
+    const { sut } = makeSut();
+    const cpf = 'invalid_value';
+    const result = sut.isValid(cpf);
+    expect(result).toBe(false);
+  });
+  it('GIVEN a sut instance WHEN validating an invalid CPF value THEN should return false', () => {
+    const { sut } = makeSut();
+    const cpf = '11111111111';
+    const result = sut.isValid(cpf);
+    expect(result).toBe(false);
+  });
+  it('GIVEN a sut instance WHEN validating an invalid CPF value THEN should return false', () => {
+    const { sut } = makeSut();
+    const cpf = '55555555555';
+    const result = sut.isValid(cpf);
+    expect(result).toBe(false);
+  });
+  it('GIVEN a sut instance WHEN validating a valid CPF value THEN should return true', () => {
+    const { sut } = makeSut();
+    const cpf = '39892942701';
+    const result = sut.isValid(cpf);
+    expect(result).toBe(true);
+  });
+  it('GIVEN a sut instance WHEN validating a valid CPF value THEN should return true', () => {
+    const { sut } = makeSut();
+    const cpf = '398.929.427-01';
+    const result = sut.isValid(cpf);
+    expect(result).toBe(true);
+  });
+  it('GIVEN a sut instance WHEN validating a valid CPF value THEN should return true', () => {
+    const { sut } = makeSut();
+    const cpf = '39892942701';
+    const result = sut.calculateCpf(cpf);
+    expect(result).toBe(true);
+  });
+  it('GIVEN a sut instance WHEN validating an invalid CPF value THEN should return false', () => {
+    const { sut } = makeSut();
+    const cpf = '39892942702';
+    const result = sut.calculateCpf(cpf);
+    expect(result).toBe(false);
+  });
+});
