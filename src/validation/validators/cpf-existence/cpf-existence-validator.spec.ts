27e5cd59b942a84fd87dbdd0d40394ef9414c82e
Nilton F J <niltonfjr086@gmail.com>
2022-02-06 23:18:34 +0000

    test: implement cpf existence validation

---
+++ src/validation/validators/cpf-existence/cpf-existence-validator.spec.ts
@@ -0,0 +1,24 @@
+import { CpfExistenceValidator } from './cpf-existence-validator';
+
+const makeSut = () => ({ sut: new CpfExistenceValidator() });
+
+describe('CpfExistenceValidator unit test', () => {
+  it('should be defined', () => {
+    const { sut } = makeSut();
+    expect(sut).toBeDefined();
+  });
+  it('GIVEN a valid existent value WHEN validating THEN should return false', async () => {
+    const { sut } = makeSut();
+    const cpfRepository = { find: () => Promise.resolve(true) } as any;
+    const cpf = 'any_valid_existent_value';
+    const result = await sut.isValid(cpfRepository, cpf);
+    expect(result).toBe(false);
+  });
+  it('GIVEN a valid but not existent value WHEN validating THEN should return true', async () => {
+    const { sut } = makeSut();
+    const cpfRepository = { find: () => Promise.resolve(false) } as any;
+    const cpf = 'any_valid_not_existent_value';
+    const result = await sut.isValid(cpfRepository, cpf);
+    expect(result).toBe(true);
+  });
+});
