93bf96f8143a2fe79a55cc8a560115fc41de6ae9
Nilton F J <niltonfjr086@gmail.com>
2022-02-07 18:51:50 +0000

    fix: remove unused tag

--- src/swagger.service.ts
+++ src/swagger.service.ts
@@ -5,7 +5,6 @@ export class SwaggerService {
     const config = new DocumentBuilder()
       .setTitle('API [Documentation]')
       .setVersion('1.0')
-      .addTag('person')
       .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
